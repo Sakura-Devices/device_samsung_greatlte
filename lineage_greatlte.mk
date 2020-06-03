8a4b7f67528bf0df0ef5b97f687d3cb67e005180
haridhayal <haridhayal@gmail.com>
2020-06-03 11:29:15 +0530

    greatlte official

--- lineage_greatlte.mk
+++ lineage_greatlte.mk
@@ -25,6 +25,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+TARGET_BOOT_ANIMATION_RES := 1440
+SAKURA_OFFICIAL=true
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := greatlte
 PRODUCT_NAME := lineage_greatlte
